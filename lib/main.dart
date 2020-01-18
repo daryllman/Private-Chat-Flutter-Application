68745771d338bffb9f5295d87211bfb2ccc2f082
Daryll Wong <daryll_wong@hotmail.com>
2020-01-18 22:42:39 +0800

    add namedRoutes

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,26 @@ import 'package:private_chat/screens/login_screen.dart';
 import 'package:private_chat/screens/registration_screen.dart';
 import 'package:private_chat/screens/chat_screen.dart';
 
-void main() => runApp(FlashChat());
+void main() => runApp(PrivateChat());
 
-class FlashChat extends StatelessWidget {
+class PrivateChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      //home: WelcomeScreen(), ---cannot use this if you are using initial route.
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id : (context)=> WelcomeScreen(),
+        LoginScreen.id : (context)=> LoginScreen(),
+        RegistrationScreen.id : (context)=> RegistrationScreen(),
+        ChatScreen.id : (context)=> ChatScreen(),
+      },
     );
   }
 }
